6ffd45b2bf8777c37e232111716d82e478b1296c
L33t <vincegreco92@gmail.com>
2018-03-24 12:55:23 -0600

    Integrated permissions to hide status from elements from non admin

--- app/dashboards/post_dashboard.rb
+++ app/dashboards/post_dashboard.rb
@@ -1,42 +1,37 @@
 require "administrate/base_dashboard"
 
 class PostDashboard < Administrate::BaseDashboard
-
   ATTRIBUTE_TYPES = {
     user: Field::BelongsTo.with_options(searchable: false),
     id: Field::Number.with_options(searchable: false),
     date: Field::DateTime.with_options(searchable: false),
-    work_performed: Field::Text.with_options(searchable: true),
+    rationale: Field::Text.with_options(searchable: true),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
     status: Field::Text.with_options(searchable: true),
   }.freeze
 
-
   COLLECTION_ATTRIBUTES = [
     :user,
     :status,
     :date,
-    :work_performed,
+    :rationale,
   ].freeze
-
 
   SHOW_PAGE_ATTRIBUTES = [
     :user,
     :status,
     :id,
     :date,
-    :work_performed,
+    :rationale,
     :created_at,
     :updated_at,
   ].freeze
 
-
   FORM_ATTRIBUTES = [
     :user,
     :date,
-    :work_performed,
+    :rationale,
   ].freeze
-
 
 end
