718f43baacaf4521a92bc902fb607b0adde2d4b5
L33t <vincegreco92@gmail.com>
2018-03-24 14:04:42 -0600

    refactored post spec test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
 	enum status: { active: 0, archived: 1 }
 	belongs_to :user 
 	validates_presence_of :date, :rationale 
+
+	scope :posts_by, ->(user) {where(user_id: user.id) }
 end
